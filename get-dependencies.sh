077a7871e73ce25349fc74d34f84820d5af119b0
Link4Electronics <link4electronics@gmail.com>
2026-08-23 19:12:44 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm libdecor
+pacman -Syu --noconfirm cmake sdl12-compat
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-make-aur-package fabother-world-git
+echo "Building Fabother World..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/fabiensanglard/Another-World-Bytecode-Interpreter"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --depth 1 "$REPO" ./Another
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
+mkdir -p ./AppDir/bin
+cd ./Another
+cmake ./ && make -j$(nproc)
+mv -v raw ../AppDir/bin/fabother-world
