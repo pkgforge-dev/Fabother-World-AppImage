8f78d72a5301b3b5e667bb25d96bdfcc91fd5988
Link4Electronics <link4electronics@gmail.com>
2026-08-29 12:12:41 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,15 +3,14 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q fabother-world-git | awk '{print $2; exit}')
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
-quick-sharun /usr/share/fabother-world/fabother-world
+quick-sharun ./AppDir/bin/fabother-world
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
