e98b0f3eef08c218fb8a77959ac3ed40cf49f904
Link4Electronics <link4electronics@gmail.com>
2026-08-29 12:07:46 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,8 +8,6 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export DESKTOP=/usr/share/applications/fabother-world.desktop
-export STARTUPWMCLASS=fabother-world
 export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
